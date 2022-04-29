e88a3e26768805b153dd22c6a38314496c1ce71f
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-04-29 11:43:32 -0400

    first phase of clock face complete

--- clock.js
+++ clock.js
@@ -8,7 +8,30 @@ drawClock(); // call function
 
 // function to draw clock
 function drawClock() {
-    ctx.arc(0,0,radius,0,2*Math.PI);  // arc method create arc/curve to create circle, params draw circle and postion it.
-    ctx.fillStyle ='White';  // color that will fill the circle, the clock will have a white face.
+    drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+    var grad; 
+    // these 4 lines draw the white circle for the face of the clock
+    ctx.beginPath(); // begins path or resets current path
+    ctx.arc(0, 0, radius, 0, 2*Math.PI);  // arc method create arc/curve to create circle, params draw circle and postion it.
+    ctx.fillStyle ='white';  // color that will fill the circle, the clock will have a white face.
     ctx.fill(); // fill the current path of the drawing, default color is black.
+
+    grad = ctx.createRadialGradient(0, 0, radius*0.95, 0, 0, radius*1.05); // creating radial gradient
+    // create 3 color stops corresponding with inner, middle, outer edge of clock
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333'); // colorstop creates 3d affect
+
+    ctx.strokeStyle = grad; // define gradient as stroke style of drawing object
+    ctx.lineWidth = radius*0.1; // define line width of drawing object
+    ctx.stroke(); // use stroke method to draw circle
+
+    // draw clock centre
+    ctx.beginPath();
+    ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
 }
